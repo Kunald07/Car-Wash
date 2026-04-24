10c08f1476b2c04a37166cfbc847d05bf3f2f10d
Kunal Deshmukh <deshmukh1207@gmail.com>
2026-04-25 00:52:47 +0530

    added js files

---
+++ script.js
@@ -0,0 +1,7 @@
+let icon = document.querySelector("#icon")
+let menuContent = document.querySelector(".menu-content")
+
+icon.addEventListener("click", function(){
+    menuContent.classList.toggle("active")
+     
+})
